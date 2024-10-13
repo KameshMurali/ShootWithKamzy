db5c9b47d68477e7c848c50effc0a5935885d096
Kameshwar Murali <kameshwarmurali@Kameshwars-MacBook-Pro.local>
2024-10-14 02:13:17 +0400

    WebPage Commit

---
+++ scripts/scripts.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title></title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="2487.5">
+  <style type="text/css">
+    body {background-color: #0f1515}
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica; -webkit-text-stroke: #000000}
+    span.s1 {font-kerning: none}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="s1">document.getElementById('contact-form').addEventListener('submit', function(e) {</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>e.preventDefault();</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>alert('Thank you for your message!');</span></p>
+<p class="p1"><span class="s1"><span class="Apple-converted-space">    </span>this.reset();</span></p>
+<p class="p1"><span class="s1">});</span></p>
+</body>
+</html>
